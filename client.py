fc887a68f341096d7e6ecbf177ce83771bf728c8
Mars Sun <marsunique@gmail.com>
2017-04-24 04:21:56 -0400

    p2s protocol

--- client.py
+++ client.py
@@ -10,67 +10,178 @@ class ClientNode(object):
         self.SERVER_NAME = server_name
         self.SERVER_PORT = 7734
         self.SERVER_ADDR = socket.gethostbyname(self.SERVER_NAME)
+        # init upload hostname and port
+        self.UPLOAD_HOSTNAME = ''
         self.UPLOAD_PORT = 60000 + random.randint(1,500)
+        # init sockets
         self.mainSocket = None
+        self.uploadSocket = None
+        self.downloadSocket = None
         print 'server name: ' + self.SERVER_NAME + \
                 '\nserver address: ' + self.SERVER_ADDR + \
                 '\nserver port: ' + str(self.SERVER_PORT)
     
+    def uploadConnect(self, conn, addr):
+        print 'Connected by', addr
+
+    
+    
+    def uploadListen(self):
+        self.uploadSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.uploadSocket.bind((self.UPLOAD_HOSTNAME, self.UPLOAD_PORT))
+        self.uploadSocket.listen(1)
+        print '\nReady to upload'
+
+        # accept connection
+        while True:
+            print '1'
+            conn, addr = self.uploadSocket.accept()
+            print '2'
+            t_uploadConnect = threading.Thread(target=self.uploadConnect, args=(conn, addr))
+            t_uploadConnect.start()
+
+
+
+    # def connectServer(self):
+    #     # create client socket
+    #     self.mainSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #     self.mainSocket.connect((self.SERVER_NAME, self.SERVER_PORT))
+    #     print 'Server is connected!'
+    #     # provide rfc numbers that are available
+    #     nums = raw_input("Please provide RFC numbers (separated by ','): ")
+    #     nums = nums.split(',')
+    #     rfc_num = []
+    #     for _num in nums:
+    #         _num = _num.strip()
+    #         rfc_num.append(int(_num))
+    #     data = pickle.dumps(['connect', self.UPLOAD_PORT, rfc_num])
+    #     # data = [cmd, upload_port, rfc_num]
+    #     self.mainSocket.send(data)
+
     def connectServer(self):
         # create client socket
         self.mainSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.mainSocket.connect((self.SERVER_NAME, self.SERVER_PORT))
         print 'Server is connected!'
-        # provide rfc numbers that are available
-        nums = raw_input("Please provide RFC numbers (separated by ','): ")
-        nums = nums.split(',')
-        rfc_num = []
-        for _num in nums:
-            _num = _num.strip()
-            rfc_num.append(_num)
-        data = pickle.dumps(['connect', self.UPLOAD_PORT, rfc_num])
-        # data = [cmd, upload_port, rfc_num]
-        self.mainSocket.send(data)
+        data = 'CONNECT %s\n' % (str(self.UPLOAD_PORT))
+        self.mainSocket.sendall(data)
 
-    def queryServer(self, cmd):
-        cmd = cmd.strip()
-        cmd = cmd.split(' ')
-        print cmd
-        if not self.mainSocket:
-            print 'Error. Connect the server first!'
-            return
-        data = pickle.dumps(cmd)
-        self.mainSocket.send(data)
+    def addSever(self):
+        # provide rfc numbers that are available
+        num = raw_input("Please provide RFC number: ")
+        title = raw_input("RFC title: ")
+        # format request data
+        line_1 = 'ADD RFC %s P2P-CI/1.0\n' % (num)
+        line_2 = 'Host: %s\n' % (socket.gethostname())
+        line_3 = 'Port: %s\n' % (self.UPLOAD_PORT)
+        line_4 = 'Title: %s\n' % (title)
+        data = line_1+line_2+line_3+line_4
+        self.mainSocket.sendall(data)
+        # receive add response
+        recv_data = self.mainSocket.recv(1024)
+        print recv_data
+
+
+    def queryServer(self):
+        data = 'QUERY\n'
+        self.mainSocket.sendall(data)
         # receive query response
         recv_data = pickle.loads(self.mainSocket.recv(1024))
+        print recv_data
+
+    def lookupServer(self):
+        num = raw_input("Please provide RFC number: ")
+        title = raw_input("RFC title: ")
+        # format request data
+        line_1 = 'LOOKUP RFC %s P2P-CI/1.0\n' % (num)
+        line_2 = 'Host: %s\n' % (socket.gethostname())
+        line_3 = 'Port: %s\n' % (self.UPLOAD_PORT)
+        line_4 = 'Title: %s\n' % (title)
+        data = line_1+line_2+line_3+line_4
+        self.mainSocket.sendall(data)
+        # receive lookup response
+        recv_data = self.mainSocket.recv(1024)
+        print recv_data
+
+    def listServer(self):
+        # format request data
+        line_1 = 'LIST ALL P2P-CI/1.0\n'
+        line_2 = 'Host: %s\n' % (socket.gethostname())
+        line_3 = 'Port: %s\n' % (self.UPLOAD_PORT)
+        data = line_1+line_2+line_3
+        self.mainSocket.sendall(data)
+        # receive lookup response
+        recv_data = self.mainSocket.recv(1024)
+        print recv_data
+
+    def quitServer(self):
+        data = 'QUIT\n'
+        self.mainSocket.sendall(data)
+
+    def testSever(self):
+        cmd = raw_input("Choose test type (bad or version): ")
+        if cmd == 'bad':
+            data = 'BAD P2P-CI/1.0\n'
+        else:
+            data = 'BAD P2P-CI/2.0\n'
+        self.mainSocket.sendall(data)
+        recv_data = self.mainSocket.recv(1024)
         print recv_data
     
     def tcpClose(self):
         self.mainSocket.close()
+        #self.uploadSocket.close()
 
     def main(self):
+        # create a thread to deal with upload socket
+        # t_uploadListen = threading.Thread(target=self.uploadListen, args=())
+        # t_uploadListen.start()
+
         while True:
             cmd = raw_input('Please enter your command: ')
             '''
             cmd has types:
-            1. connect (provide register info)
-                - data format: ['connect', upload_port, rfc_num]
-            2. download (provide RFC number)
-                - data format: ['download', self_defined_protocol]
+            1. connect
+                - data format: 'CONNECT UPLOAD_PORT\n'
+            2. add
+                - data format: P2S application layer protocol
             3. query
-                -data format: ['query', rfc_num(optional)]
-            4. quit
-                -data format: ['quit']
+                - data format: 'QUERY\n'
+            4. lookup
+                - data format: P2S application layer protocol
+            5. list
+                - data format: P2S application layer protocol
+            6. quit
+                - data format: 'QUIT\n'
+            7. download
+                - this cmd doesn't need to be transmited to server, handled locally
             '''
             if cmd == 'connect':
-                self.connectServer()
-            if 'query' in cmd:
-                self.queryServer(cmd)
-            if cmd == 'quit':
-                self.mainSocket.send(pickle.dumps(['quit']))
-                self.tcpClose()
-                print 'Connection terminated'
-                break
+                if not self.mainSocket:
+                    self.connectServer()
+                else:
+                    print 'You have already connected the server'
+            else:
+                if not self.mainSocket:
+                    print 'Error. Connect the server first!'
+                    continue
+                elif cmd == 'add':
+                    self.addSever()
+                elif cmd == 'query':
+                    self.queryServer()
+                elif cmd == 'lookup':
+                    self.lookupServer()
+                elif cmd == 'list':
+                    self.listServer()
+                elif cmd == 'quit':
+                    self.quitServer()
+                    self.tcpClose()
+                    print 'Connection terminated'
+                    break
+                elif cmd == 'test':
+                    self.testSever()
+                else:
+                    print 'Error. Invalid Command!'
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -11,55 +11,117 @@ class ServerNode(object):
         self.rfc_index = {}
 
     def handleRequest(self, data, conn, addr):
-        cmd = data[0]
-        if cmd == 'connect':
+        data = data.split('\n')
+        # check version
+        # if not data[0].split(' ')[-1].strip() == 'P2P-CI/1.0':
+        #     print data[0].split(' ')[-1]
+        #     data = 'P2P-CI/1.0 505 P2P-CI Version Not Supported\n'
+        #     conn.send(data)
+        #     return False
+
+        # interpret the method
+        cmd = data[0].split(' ')[0]
+        if cmd == 'CONNECT':
             # register peer information
-            _upload_port = data[1]
-            _rfc_num = data[2]
-            self.peer_info[addr] = (_upload_port, _rfc_num)
-            for _num in _rfc_num:
-                self.rfc_index.setdefault(_num, [])
-                self.rfc_index[_num].append(addr)
+            _upload_port = int(data[0].split(' ')[1])
+            self.peer_info[addr] = [_upload_port, {}]
+        
+        
+        elif cmd == 'ADD':
+            # add rfc
+            _num = data[0].split(' ')[2]
+            _host = data[1].split(' ')[1]
+            _upload_port = data[2].split(' ')[1]
+            _title = data[3].split(' ')[1]
+            self.rfc_index.setdefault(_num, [])
+            self.rfc_index[_num].append((_host, _upload_port, _title))
+            # updata peer_info, self.peer_info[addr] = [_upload_port, {}]
+            self.peer_info[addr][1][_num] = (_host, _upload_port, _title)
+            # echo
+            data = 'P2P-CI/1.0 200 OK\n'
+            data += 'RFC %s %s %s %s\n' % (_num, _title, _host, _upload_port)
+            conn.sendall(data)
 
-        elif cmd == 'query':
-            # send back requested information
-            # query has two types: query or query with rfc number
-            if len(data) == 1:
-                # type 1
-                data = pickle.dumps(self.peer_info)
-                conn.sendall(data)
+
+        elif cmd == 'QUERY':
+            # send back whole peer information
+            # this is not requested by the project doc
+            data = pickle.dumps(self.peer_info)
+            conn.sendall(data)
+            # else:
+            #     # type 2
+            #     _rfc_num = int(data[1])
+            #     if not _rfc_num in self.rfc_index:
+            #         # rfc number is not stored in the rfc_index
+            #         data = pickle.dumps('RFC not available')
+            #     else: 
+            #         data = pickle.dumps(self.rfc_index[_rfc_num])
+            #     conn.sendall(data)
+
+
+        elif cmd == 'LOOKUP':
+            # find peers have the specified rfc
+            _num = data[0].split(' ')[2]
+            if not _num in self.rfc_index:
+                _status_code = '404'
+                _phrase = 'Not Found'
+                data = 'P2P-CI/1.0 %s %s\n' % (_status_code, _phrase)
             else:
-                # type 2
-                _rfc_num = data[1]
-                if not data[1] in self.rfc_index:
-                    # rfc number is not stored in the rfc_index
-                    data = pickle.dumps('RFC not available')
-                else: 
-                    data = pickle.dumps(self.rfc_index[_rfc_num])
-                conn.sendall(data)
+                _status_code = '200'
+                _phrase = 'OK'
+                data = 'P2P-CI/1.0 %s %s\n' % (_status_code, _phrase)
+                for record in self.rfc_index[_num]:
+                    #record = (_host, _upload_port, _title)
+                    data += 'RFC %s %s %s %s\n' % (_num, record[2], record[0], record[1])
+            conn.sendall(data)
 
-        elif cmd == 'quit':
+        
+        elif cmd == 'LIST':
+            # list all rfc index
+            if not self.rfc_index:
+                _status_code = '404'
+                _phrase = 'Not Found'
+                data = 'P2P-CI/1.0 %s %s\n' % (_status_code, _phrase)
+            else:
+                _status_code = '200'
+                _phrase = 'OK'
+                data = 'P2P-CI/1.0 %s %s\n' % (_status_code, _phrase)
+                for _num in self.rfc_index:
+                    for record in self.rfc_index[_num]:
+                        #record = (_host, _upload_port, _title)
+                        data += 'RFC %s %s %s %s\n' % (_num, record[2], record[0], record[1])
+            conn.sendall(data)
+
+
+        elif cmd == 'QUIT':
             _info = self.peer_info.pop(addr)
-            # info = (_upload_port, _rfc_num), _rfc_num is a list
+            # info = [_upload_port, _rfc_num], _rfc_num is a dictionary
             for _num in _info[1]:
-                self.rfc_index[_num].remove(addr)
+                record = _info[1][_num]
+                self.rfc_index[_num].remove(record)
                 if not self.rfc_index[_num]:
                     # index is empty
                     self.rfc_index.pop(_num)
+                    # return True used to close the socket
+                    return True
 
+        # else:
+        #     data = 'P2P-CI/1.0 400 Bad Request\n'
+        #     conn.send(data)
+        return False
 
 
 
     def connectClient(self, conn, addr):
         print 'Connected by', addr
         while True:
-            data = pickle.loads(conn.recv(1024))
-            print data
-            self.handleRequest(data, conn, addr)
-            if data[0] == 'quit':
+            data = conn.recv(1024)
+            print '\nrequest:'
+            print data,
+            print 'from', addr
+            isQuit = self.handleRequest(data, conn, addr)
+            if isQuit:
                 break
-
-            
         conn.close()
     
     def main(self):
